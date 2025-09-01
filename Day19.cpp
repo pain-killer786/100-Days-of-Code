6c7f1fd09e04925ddf27e22a746f7c81b735ccef
{pain-killer786} <{krishnendunandy43@gmail.com}>
2025-09-01 14:04:52 +0530

    Add C++ program to check whether a number is a perfect square

---
+++ Day19.cpp
@@ -0,0 +1,28 @@
+// Write a code to check whether a number is perfect square or not. 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPerfectSquare(long double x)
+{
+    if(x>=0)
+    {
+        long long sr = sqrt(x);
+        return (sr*sr == x);
+    }
+    return false;
+}
+
+int main()
+{
+    long long x= 84;
+    if(isPerfectSquare(x))
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+    return 0;
+}
