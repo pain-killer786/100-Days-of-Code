35cad553e410c1a179c80e0396de713b7ffd3ee4
{pain-killer786} <{krishnendunandy43@gmail.com}>
2025-09-01 14:05:01 +0530

    Add C++ program to find the power of a number using recursion

---
+++ Day20.cpp
@@ -0,0 +1,19 @@
+//Write a program to find the power of number using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int power(int base, int x)
+{
+    if(x==0)
+    {
+        return 1;
+    }
+    return (base*power(base,x-1));
+}
+
+int main()
+{
+    int base = 5, x=3;
+    cout<<"Required Power is"<<power(base, x);
+}
